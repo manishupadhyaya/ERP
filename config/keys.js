83e8ed5552a6b233e47ccea6a774f6486960d903
manishupadhyaya <32922206+manishupadhyaya@users.noreply.github.com>
2021-01-14 16:04:38 +0530

    Updated keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb://manish:erp2019@ds157853.mlab.com:57853/node-rest-api'
+    mongoURI: 'mongodb://manish:erp2019@cluster0-shard-00-00.oekpl.mongodb.net:27017,cluster0-shard-00-01.oekpl.mongodb.net:27017,cluster0-shard-00-02.oekpl.mongodb.net:27017/ERP?ssl=true&replicaSet=atlas-3civah-shard-0&authSource=admin&retryWrites=true&w=majority'
   };
